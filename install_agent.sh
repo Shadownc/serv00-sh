053427453d9cfbb94c0ca4e17a07ae0b1ac43fff
anonymous <50570049+xsj684135@users.noreply.github.com>
2024-08-22 08:20:10 +0800

    Update install_agent.sh

--- install_agent.sh
+++ install_agent.sh
@@ -2,13 +2,13 @@
 
 # 下载并解压 Nezha agent
 wget -O ~/nezha-agent_freebsd_amd64.zip https://github.com/nezhahq/agent/releases/download/v0.16.7/nezha-agent_freebsd_amd64.zip
-unzip ~/nezha-agent_freebsd_amd64.zip -d ~/domains/USERNAME.serv00.com/public_html/nezhapanel
+unzip ~/nezha-agent_freebsd_amd64.zip -d ~/domains/gulang.serv00.net/public_html/nezhapanel
 
 # 创建 tmp 目录
 mkdir -p ~/tmp
 
 # 切换到 nezhapanel 目录
-cd ~/domains/USERNAME.serv00.com/public_html/nezhapanel
+cd ~/domains/gulang.serv00.net/public_html/nezhapanel
 
 # 赋予 nezha-agent 可执行权限
 chmod +x nezha-agent
@@ -17,9 +17,9 @@ chmod +x nezha-agent
 cat > agent.sh << EOF
 #!/bin/sh
 
-export TMPDIR=/home/USERNAME/tmp
+export TMPDIR=/home/gulang/tmp
 
-/home/USERNAME/domains/USERNAME.serv00.com/public_html/nezhapanel/nezha-agent -s USERNAME.serv00.com:13141 -p password -d
+/home/gulang/domains/gulang.serv00.net/public_html/nezhapanel/nezha-agent -s tz.xsjcloud.cn:47321 -p qPbBWP6kVChoJNdtiX -d
 EOF
 
 # 赋予 agent.sh 可执行权限
